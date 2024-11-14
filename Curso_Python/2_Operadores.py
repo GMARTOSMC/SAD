8e0351e35d488421ece68252ee62c6a085fde599
GonzaloMartos <gmartos96@tutamail.com>
2024-11-14 16:00:22 +0100

    python

---
+++ Curso_Python/2_Operadores.py
@@ -0,0 +1,58 @@
+### Operadores Aritméticos ###
+
+### Operaciones de enteros ###
+
+### Suma de dos números ###
+
+print(3+5)
+
+### declarar variables por ejemplo variable "paco" ###
+
+paco = "pocholo"
+
+print(paco)
+
+### Resta ###
+
+print(5-2)
+
+### Multiplicacion ###
+
+print(2*4)
+
+### División ###
+
+print(7/2)
+
+### Modulo ###
+
+print(12%5)
+
+### División sin decimales ###
+
+print(40//3)
+
+### Texto con string transformas numeros en texto" 
+
+print("Hola " + str(5))
+
+### elevar un numero a otro ###
+
+print(3**5)
+
+### Si multiplicas por float la varaible será float ###
+
+my_float = 2.5 * 2
+
+### La podemos convertir a entero ###
+
+print("Hola " + str(int(my_float)))
+
+### Comparativos ###
+
+print(3>4)
+print(3<4)
+print(3<=4)
+print(3>=4)
+print(3 == 4)
+print(3!=4)
